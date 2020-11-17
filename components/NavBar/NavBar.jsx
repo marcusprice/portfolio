969e2f652599693e064d32a2574427e4f2e8632f
Price <marcusprice88@gmail.com>
2020-11-17 14:15:00 -0800

    finished nav bar styling

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,7 +1,15 @@
+import styles from './NavBar.module.css';
+
 export default function() {
     return(    
-        <nav>
-            <h1>suh dude</h1>
+        <nav className={styles.container}>
+            <h1>Marcus Price</h1>
+            <ul className={styles.navItems}>
+                <li>About</li>
+                <li>Portfolio</li>
+                <li>Blog</li>
+                <li>Contact</li>
+            </ul>
         </nav>
     );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import NavBar from '../components/NavBar/';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="app-container">
-      <NavBar />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;500;600;700;900&display=swap" rel="stylesheet" />
+      </Head>
+      <div className="app-container">
+        <NavBar />
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
 
