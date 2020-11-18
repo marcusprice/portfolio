6193cad669eb6c377d033ccd76517c7508d434e2
Price <marcusprice88@gmail.com>
2020-11-17 17:18:54 -0800

    finished nav tiles

---
+++ components/NavTiles/NavTiles.jsx
@@ -0,0 +1,31 @@
+import styles from './NavTiles.module.css';
+
+export default function NavTiles() {
+    return(
+        <div className={styles.container}>
+            <div style={{backgroundImage: 'url("./images/mountains-min.jpg")'}}>
+                <div>
+                    About
+                </div>
+            </div>
+
+            <div style={{backgroundImage: 'url("./images/code-min.jpg")'}}>
+                <div>
+                    Portfolio
+                </div>
+            </div>
+
+            <div style={{backgroundImage: 'url("./images/blog-min.jpg")'}}>
+                <div>
+                    Blog
+                </div>
+            </div>
+
+            <div style={{backgroundImage: 'url("./images/message-min.jpg")'}}>
+                <div>
+                    Contact
+                </div>
+            </div>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import ContentSection from '../components/ContentSection';
 import SectionHeader from '../components/SectionHeader/';
+import NavTiles from '../components/NavTiles';
 
 export default function Home() {
   return (
     <>
       <ContentSection>
-        <SectionHeader title="I'm Marcus Price, a software engineer based out of Seattle, WA" />
+        <SectionHeader title="I'm Marcus Price, a software engineer based out of Seattle, WA" cta="Get in touch"/>
       </ContentSection>
       <ContentSection>
-        <SectionHeader title="I'm Marcus Price, a software engineer based out of Seattle, WA" />
+        <NavTiles />
       </ContentSection>
     </>
   );
